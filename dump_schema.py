8d12ca21ab69ef184e0eef3dd347cecda6100a3d
23f2004440 <23f2004440@ds.study.iitm.ac.in>
2026-03-09 20:52:30 +0530

    Delete dump_schema.py

--- dump_schema.py
+++
@@ -1,6 +0,0 @@
-import sqlite3
-conn = sqlite3.connect('backend/ppa.db')
-with open('schema.txt', 'w') as f:
-    for row in conn.execute("SELECT sql FROM sqlite_master WHERE type='table'"):
-        if row[0]:
-            f.write(row[0] + '\n\n')
